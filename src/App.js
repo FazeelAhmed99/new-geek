e0f9e3631efc002d1cc181a8071a9fc902254ff4
Fazeel <fazeelahmedkhn99@gmail.com>
2021-11-16 16:03:42 +0500

    second commit header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Header from './components/Header'
-const App = () => {
+import React from 'react';
+import Header from './components/Header';
+// import Body1 from './components/Body1';
+// import Body3 from './components/Body3';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Body2 from './components/Body2';
+import Pexels from './Pexels.mp4'
+import './App.css'
+
+function App() {
   return (
     <>
-    <Header />
-      
+
+    <video className="video" autoPlay muted loop>
+      <source src={Pexels} type="video/mp4"/>
+    </video>
+    <Header/>
+    {/* <Body1/>
+    <Body2 />
+    <Body3/>
+    <Contact/>
+    <Footer/> */}
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
